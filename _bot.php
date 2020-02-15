0f1a8a905bccd88e1946e8f2daf34620a752e221
Austin Wright <awguitar86@gmail.com>
2020-02-14 22:28:25 -0700

    adjust js for active class

--- _bot.php
+++ _bot.php
@@ -103,8 +103,8 @@
 
     $(document).ready(function () {
         var url = window.location;
-        $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
-        $('ul.nav a').filter(function() {
+        $('ul li a[href="'+ url +'"]').parent().addClass('active');
+        $('ul li a').filter(function() {
              return this.href == url;
         }).parent().addClass('active');
     });
