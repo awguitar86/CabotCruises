95cfee2a6988b568d07086212cc29a1bb9902174
Austin Wright <awguitar86@gmail.com>
2020-02-19 11:01:52 -0700

    added wordpress

--- index.php
+++ index.php
@@ -1,50 +1,17 @@
 <?php
-    $title="Home";
-    include '_top.php';
-?>
-        <div class="hero-sm">
-            <div><img src="./images/cruiseShipSm1.jpg" alt="Cruise Ship" class="heroSm1"></div>
-            <div><img src="./images/cruiseShipSm2.jpg" alt="Cruise Ship" class="heroSm2"></div>
-            <div><img src="./images/cruiseShipSm3.jpg" alt="Cruise Ship" class="heroSm3"></div>
-            <div><img src="./images/cruiseShipSm4.jpg" alt="Cruise Ship" class="heroSm4"></div>
-        </div>
-        <div class="hero-lg">
-            <div><img src="./images/cruiseShip1.jpg" alt="Cruise Ship" class="hero1"></div>
-            <div><img src="./images/cruiseShip2.jpg" alt="Cruise Ship" class="hero2"></div>
-            <div><img src="./images/cruiseShip3.jpg" alt="Cruise Ship" class="hero3"></div>
-            <div><img src="./images/cruiseShip4.jpg" alt="Cruise Ship" class="hero4"></div>
-        </div>
-        <div class="blog-articles">
-            <h2>Blog Articles</h2>
-            <div class="blog-teasers">
-                <div class="blogItem">
-                    <a href="#" class="blogLink">
-                        <img class="blogImg" src="./images/blog1.jpg" alt="blog picture">
-                        <h4 class="blogH4">Tropical Vacation</h4>
-                        <p class="blogP">April 2, 2019</p>
-                    </a>
-                </div>
-                <div class="blogItem">
-                    <a href="#" class="blogLink">
-                        <img class="blogImg" src="./images/blog2.jpg" alt="blog picture">
-                        <h4 class="blogH4">Take A Vacation</h4>
-                        <p class="blogP">May 2, 2019</p>
-                    </a>
-                </div>
-                <div class="blogItem">
-                    <a href="#" class="blogLink">
-                        <img class="blogImg" src="./images/blog3.jpg" alt="blog picture">
-                        <h4 class="blogH4">Vacation Spots</h4>
-                        <p class="blogP">June 2, 2019</p>
-                    </a>
-                </div>
-                <div class="blogItem">
-                    <a href="#" class="blogLink">
-                        <img class="blogImg" src="./images/blog4.jpg" alt="blog picture">
-                        <h4 class="blogH4">The Cruise Ship</h4>
-                        <p class="blogP">July 2, 2019</p>
-                    </a>
-                </div>
-            </div>
-        </div>
-<?php include '_bot.php'; ?>
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
