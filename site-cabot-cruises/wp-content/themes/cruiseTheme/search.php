31fe8e5c24c5a465e3183af5160963c89cb1d3ef
Austin Wright <awguitar86@gmail.com>
2020-02-28 13:05:12 -0700

    changed heading tags

--- site-cabot-cruises/wp-content/themes/cruiseTheme/search.php
+++ site-cabot-cruises/wp-content/themes/cruiseTheme/search.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
-
+<h1>Search Results</h1>
 <?php while ( have_posts() ) : the_post(); ?>
-  <?php the_title( '<h1>', '</h1>'); ?>
+  <?php the_title( '<h2>', '</h2>'); ?>
   <?php the_excerpt(); ?>
 <?php endwhile; ?>
 
