5fc4549e576c4ac52f8ed0bcf4556465faf90861
Austin Wright <awguitar86@gmail.com>
2020-02-28 13:50:13 -0700

    added div and styled it for search results

--- site-cabot-cruises/wp-content/themes/cruiseTheme/search.php
+++ site-cabot-cruises/wp-content/themes/cruiseTheme/search.php
@@ -1,8 +1,10 @@
 <?php get_header(); ?>
-<h1 class="search-results-h1">Search Results</h1>
-<?php while ( have_posts() ) : the_post(); ?>
-  <?php the_title( '<h2 class="search-results-h2">', '</h2>'); ?>
-  <?php the_excerpt('<p class="search-results-p">', '</p>'); ?>
-<?php endwhile; ?>
+<div class="search-results">
+  <h1 class="search-results-h1">Search Results</h1>
+  <?php while ( have_posts() ) : the_post(); ?>
+    <?php the_title( '<h2 class="search-results-h2">', '</h2>'); ?>
+    <?php the_excerpt(); ?>
+  <?php endwhile; ?>
+</div>
 
 <?php get_footer() ?>
