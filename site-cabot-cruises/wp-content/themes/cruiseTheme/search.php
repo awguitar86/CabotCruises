efd86f330d966e4efbb2f8b30bfa38bd0bd88a02
Austin Wright <awguitar86@gmail.com>
2020-02-28 13:30:27 -0700

    added styling

--- site-cabot-cruises/wp-content/themes/cruiseTheme/search.php
+++ site-cabot-cruises/wp-content/themes/cruiseTheme/search.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <h1 style="width:100%;">Search Results</h1>
 <?php while ( have_posts() ) : the_post(); ?>
-  <?php the_title( '<h2 style="width:100%;">', '</h2>'); ?>
+  <?php the_title( '<h2 style="width:100%; margin:0px 0px 5px 0px;">', '</h2>'); ?>
   <?php the_excerpt(); ?>
 <?php endwhile; ?>
 
