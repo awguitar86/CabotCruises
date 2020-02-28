d0f14abf02fca40f182049f74481f813c4dbcb24
Austin Wright <awguitar86@gmail.com>
2020-02-28 13:20:43 -0700

    aded function

--- site-cabot-cruises/wp-content/themes/cruiseTheme/functions.php
+++ site-cabot-cruises/wp-content/themes/cruiseTheme/functions.php
@@ -5,6 +5,10 @@ add_filter( 'user_can_richedit', '__return_false', 50);
 // create a menu
 register_nav_menu('siteNavigation', 'Primary Navigation');
 
-
+// Read More Button
+function wpdocs_excerpt_more( $more ) {
+  return '<a href="'.get_the_permalink().'" rel="nofollow">Read More...</a>';
+}
+add_filter( 'excerpt_more', 'wpdocs_excerpt_more' );
 
 ?>
