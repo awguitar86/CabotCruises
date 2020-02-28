5fcdaf9e4a2d56403b23d84339280a2b47a90211
Austin Wright <awguitar86@gmail.com>
2020-02-28 13:45:47 -0700

    added p tag and styled it

--- site-cabot-cruises/wp-content/themes/cruiseTheme/search.php
+++ site-cabot-cruises/wp-content/themes/cruiseTheme/search.php
@@ -2,7 +2,7 @@
 <h1 class="search-results-h1">Search Results</h1>
 <?php while ( have_posts() ) : the_post(); ?>
   <?php the_title( '<h2 class="search-results-h2">', '</h2>'); ?>
-  <?php the_excerpt(); ?>
+  <?php the_excerpt('<p class="search-results-p>', '</p>'); ?>
 <?php endwhile; ?>
 
 <?php get_footer() ?>
