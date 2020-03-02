66aac0182cffc160419a9da05984265ea2d4fa62
Austin Wright <awguitar86@gmail.com>
2020-03-02 10:37:08 -0700

    added happy hogan image and php file

---
+++ site-cabot-cruises/wp-content/themes/cruiseTheme/agentHappy.php
@@ -0,0 +1,53 @@
+<?php
+    $title="Agent Tony Stark";
+    include '_top.php';
+?>
+  <div class="agent">
+    <h1>Agent Happy Hogan</h1>
+    <div class="agent-content">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/happyHogan.jpg" alt="Happy Hogan">
+      <p>
+        <strong>Phone:</strong>
+        <br/>
+        1-800-555-6667
+      </p>
+      <p>
+        <strong>Working:</strong>
+        <br/>
+        Monday-Friday
+      </p>
+      <p>
+        <strong>Destination Specialization:</strong>
+        <br/>
+        Europe, Bahamas, Hawaii, California, Mexico, Central America, New Zealand.
+      </p>
+      <p>
+        <strong>Office Hours:</strong>
+        <br/>
+        9:00am - 6:00pm
+      </p>
+      <p>
+        <strong>Biography:</strong>
+        <br/>
+        I've had to travel a lot because of my past work experience. After I retired from that, I thought I would do something fun and help people make their vacations the best vacation possible.
+        <br/>
+        <br/>
+        I started traveling when I started working for Tony Stark. I was his assistant and I went with him everywhere, and he traveled everywhere. I got to see some really neat stuff, and now I want to help others see some really neat stuff!
+      </p>
+      <p>
+        <strong>Customer Comment:</strong>
+        <br/>
+        "Happy truely knows his way around so many awesome places. He helped us bypass so many things so that our vacation would be smooth and stress free. Thank you Happy!"<br/>
+        -Joe and Trudy Drupal
+        <br/>
+        <br/>
+        "My vacation was the best because Happy knew exactly where to go to make it the best. Never had a better vacation in my life!"<br/>
+        -Betty Roberts
+        <br/>
+        <br/>
+        "Absolutely amazing! Every second of my cruise was in heaven! Thank you Happy for your help in finding the best place for my husband and I!"<br/>
+        -Suzann Thorp
+      </p>
+    </div>
+  </div>
+<?php include '_bot.php'; ?>
