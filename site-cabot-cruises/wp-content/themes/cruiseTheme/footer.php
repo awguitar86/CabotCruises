8d0d78283a7225ba06f0109a75b796e2258ae1f6
Austin Wright <awguitar86@gmail.com>
2020-03-02 11:47:28 -0700

    changed agenMenu to get by id

--- site-cabot-cruises/wp-content/themes/cruiseTheme/footer.php
+++ site-cabot-cruises/wp-content/themes/cruiseTheme/footer.php
@@ -49,7 +49,7 @@
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="  crossorigin="anonymous"></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/slick/slick/slick.min.js"></script>
 <script>
-    const agentMenu = document.getElementsByClassName('agent-menu');
+    const agentMenu = document.getElementById('menu-item-52').firstElementChild;
     console.log(agentMenu);
     // agentMenu.id = 'agent-menu-link';
     const hamburger = document.getElementById("menu");
