a3a7762521d0a2fbe46bbb8c5d73a4dab626da9b
Austin Wright <awguitar86@gmail.com>
2020-03-31 14:16:21 -0600

    changed code in index.php

--- site-cabot-cruises/wp-content/themes/cruiseTheme/index.php
+++ site-cabot-cruises/wp-content/themes/cruiseTheme/index.php
@@ -1,52 +1,8 @@
+<?php get_header(); ?>
 
-        <div class="hero-slider">
-            <picture>
-                <source srcset="/images/cruiseShip1.jpg" media="(min-width: 680px)" />
-                <img srcset="/images/cruiseShipSm1.jpg" alt="Cruise Ship">
-            </picture>
-            <picture>
-                <source srcset="/images/cruiseShip2.jpg" media="(min-width: 680px)" />
-                <img srcset="/images/cruiseShipSm2.jpg" alt="Cruise Ship">
-            </picture>
-            <picture>
-                <source srcset="/images/cruiseShip3.jpg" media="(min-width: 680px)" />
-                <img srcset="/images/cruiseShipSm3.jpg" alt="Cruise Ship">
-            </picture>
-            <picture>
-                <source srcset="/images/cruiseShip4.jpg" media="(min-width: 680px)" />
-                <img srcset="/images/cruiseShipSm4.jpg" alt="Cruise Ship">
-            </picture>
-        </div>
-        <div class="blog-articles">
-            <h2>Blog Articles</h2>
-            <div class="blog-teasers">
-                <div class="blogItem">
-                    <a href="#" class="blogLink">
-                        <img class="blogImg" src="<?php echo get_template_directory_uri(); ?>/images/blog1.jpg" alt="blog picture">
-                        <h4 class="blogH4">Tropical Vacation</h4>
-                        <p class="blogP">April 2, 2019</p>
-                    </a>
-                </div>
-                <div class="blogItem">
-                    <a href="#" class="blogLink">
-                        <img class="blogImg" src="<?php echo get_template_directory_uri(); ?>/images/blog2.jpg" alt="blog picture">
-                        <h4 class="blogH4">Take A Vacation</h4>
-                        <p class="blogP">May 2, 2019</p>
-                    </a>
-                </div>
-                <div class="blogItem">
-                    <a href="#" class="blogLink">
-                        <img class="blogImg" src="<?php echo get_template_directory_uri(); ?>/images/blog3.jpg" alt="blog picture">
-                        <h4 class="blogH4">Vacation Spots</h4>
-                        <p class="blogP">June 2, 2019</p>
-                    </a>
-                </div>
-                <div class="blogItem">
-                    <a href="#" class="blogLink">
-                        <img class="blogImg" src="<?php echo get_template_directory_uri(); ?>/images/blog4.jpg" alt="blog picture">
-                        <h4 class="blogH4">The Cruise Ship</h4>
-                        <p class="blogP">July 2, 2019</p>
-                    </a>
-                </div>
-            </div>
-        </div>
+<?php while ( have_posts() ) : the_post(); ?>
+  <?php the_title( '<h1>', '</h1>'); ?>
+  <?php the_content(); ?>
+<?php endwhile; ?>
+
+<?php get_footer() ?>
