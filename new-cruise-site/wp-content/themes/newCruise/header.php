4ba9e4236becdecd81e54c40d473bb454644ab1a
Austin Wright <awguitar86@gmail.com>
2020-04-18 13:30:02 -0600

    nav bar

--- new-cruise-site/wp-content/themes/newCruise/header.php
+++ new-cruise-site/wp-content/themes/newCruise/header.php
@@ -21,20 +21,7 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/about">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/features">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/contact">Contact</a>
-        </li>
-      </ul>
+      <?php wp_nav_menu()?>
     </div>
   </nav>
 
