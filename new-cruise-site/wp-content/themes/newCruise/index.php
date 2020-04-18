35938830a5f744c938ea513d271c9856c9b260d6
Austin Wright <awguitar86@gmail.com>
2020-04-18 16:41:31 -0600

    carousel styling

--- new-cruise-site/wp-content/themes/newCruise/index.php
+++ new-cruise-site/wp-content/themes/newCruise/index.php
@@ -5,14 +5,14 @@
   <?php the_content(); ?>
 <?php endwhile; ?>
 
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+<div id="carouselExampleIndicators" class="carousel slide w-100" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
   </ol>
-  <div class="carousel-inner">
+  <div class="carousel-inner w-100">
     <div class="carousel-item active">
       <img src="<?php echo get_template_directory_uri(); ?>/images/cruiseShip1.jpg" class="d-block w-100" alt="Cruise Ship">
     </div>
